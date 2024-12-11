fd752d2e9e848c93614be8ce162ab358afcc8a06
Pedro B Cavalcante <pedrobenevides3243@gmail.com>
2024-12-11 17:22:23 -0300

    adicionado firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:blog/app_module.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
   runApp(ModularApp(module: AppModule(), child: const MyApp()));
 }
 
