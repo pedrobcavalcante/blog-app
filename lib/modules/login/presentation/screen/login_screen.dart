ac66a95e0745c7c8c113e5d9a5d01379ac584c49
Pedro B Cavalcante <pedrobenevides3243@gmail.com>
2024-12-16 22:20:02 -0300

    bugfix loginscreen

--- lib/modules/login/presentation/screen/login_screen.dart
+++ lib/modules/login/presentation/screen/login_screen.dart
@@ -18,155 +18,135 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController(text: 'teste@email.com');
+  final passwordController = TextEditingController(text: '12345678');
+  final bloc = Modular.get<LoginBloc>();
+
   @override
   void dispose() {
     Modular.dispose<LoginBloc>();
+    emailController.dispose();
+    passwordController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final formKey = GlobalKey<FormState>();
-    final emailController = TextEditingController(text: 'teste@email.com');
-    final passwordController = TextEditingController(text: '12345678');
-    final bloc = Modular.get<LoginBloc>();
-    return BlocProvider(
-      create: (context) => bloc,
-      child: Scaffold(
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: size.height * 0.3,
         backgroundColor: Colors.blue,
-        body: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.4,
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: const Center(
-                child: Text(
-                  'Blog App',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+        title: const Center(
+          child: Text(
+            'Blog App',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
             ),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      blurRadius: 10,
-                      offset: const Offset(10, 15),
-                    ),
-                  ],
+          ),
+        ),
+      ),
+      backgroundColor: Colors.blue,
+      body: Container(
+        alignment: Alignment.center,
+        height: size.height,
+        padding: const EdgeInsets.all(16.0),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                BlogTextField(
+                  controller: emailController,
+                  hintText: "E-mail",
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira seu email';
+                    }
+                    return null;
+                  },
                 ),
-                child: Center(
-                  child: SingleChildScrollView(
-                    child: Container(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Form(
-                        key: formKey,
-                        child: Column(
-                          children: [
-                            BlogTextField(
-                              controller: emailController,
-                              hintText: "E-mail",
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Por favor, insira seu email';
-                                }
-                                return null;
-                              },
-                            ),
-                            const SizedBox(height: 20),
-                            BlogTextField(
-                              hintText: "Senha",
-                              controller: passwordController,
-                              isPassword: true,
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Por favor, insira sua senha';
-                                }
-                                return null;
-                              },
-                            ),
-                            const SizedBox(height: 20),
-                            BlocConsumer<LoginBloc, LoginState>(
-                              listener: (context, state) {
-                                if (state is LoginSuccess) {
-                                  Modular.to.pushReplacementNamed(HomeScreen.routeName);
-                                }
-                              },
-                              builder: (context, state) {
-                                if (state is LoginInProgress) {
-                                  return const CircularProgressIndicator();
-                                }
-                                return Column(
-                                  children: [
-                                    BlogButton(
-                                      text: "Login",
-                                      onPressed: () {
-                                        if (formKey.currentState!.validate()) {
-                                          bloc.add(
-                                            LoginRequested(
-                                              email: emailController.text,
-                                              password: passwordController.text,
-                                            ),
-                                          );
-                                        }
-                                      },
-                                    ),
-                                    state is LoginFailure
-                                        ? Padding(
-                                            padding:
-                                                const EdgeInsets.only(top: 20),
-                                            child: Text(
-                                              state.errorMessage,
-                                              style: const TextStyle(
-                                                color: Colors.red,
-                                                fontSize: 16,
-                                              ),
-                                              textAlign: TextAlign.center,
-                                            ),
-                                          )
-                                        : const SizedBox(),
-                                    const SizedBox(height: 40),
-                                    TextButton(
-                                      onPressed: () {
-                                        Modular.to.pushNamed(
-                                            RegisterScreen.routeName);
-                                      },
-                                      child: const Text(
-                                        'Ainda não possui uma conta? Registre-se',
-                                        style: TextStyle(
-                                          color: Colors.blue,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                );
-                              },
-                            ),
-                          ],
+                const SizedBox(height: 20),
+                BlogTextField(
+                  hintText: "Senha",
+                  controller: passwordController,
+                  isPassword: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira sua senha';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                BlocConsumer<LoginBloc, LoginState>(
+                  bloc: bloc,
+                  listener: (context, state) {
+                    if (state is LoginSuccess) {
+                      Modular.to.pushReplacementNamed(HomeScreen.routeName);
+                    }
+                  },
+                  builder: (context, state) {
+                    if (state is LoginInProgress) {
+                      return const CircularProgressIndicator();
+                    }
+                    return Column(
+                      children: [
+                        BlogButton(
+                          text: "Login",
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              bloc.add(LoginRequested(
+                                email: emailController.text,
+                                password: passwordController.text,
+                              ));
+                            }
+                          },
                         ),
-                      ),
-                    ),
-                  ),
+                        if (state is LoginFailure)
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Text(
+                              state.errorMessage,
+                              style: const TextStyle(
+                                color: Colors.red,
+                                fontSize: 16,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        const SizedBox(height: 40),
+                        TextButton(
+                          onPressed: () {
+                            Modular.to.pushNamed(RegisterScreen.routeName);
+                          },
+                          child: const Text(
+                            'Ainda não possui uma conta? Registre-se',
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
